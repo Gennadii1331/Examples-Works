9ef5f346d6ca2b3356fda22167eee7cb59228344
student <g.kovtonyk@gmail.com>
2018-04-28 14:55:38 +0300

    lesson-15

---
+++ src/lesson-15/component/taskList/task-list.js
@@ -0,0 +1,25 @@
+export class TaskList {
+    constructor(element) {
+        this.rootElement = element;
+        this.fetchData();
+    }
+
+    fetchData() {
+        const xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'http://localhost:4001/list');
+
+        xhr.send();
+
+        xhr.addEventListener('readystatechange', () => {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                this.render(xhr.response);
+            }
+        });
+    }
+
+
+    render(data) {
+        console.log(data);
+    }
+}
--- src/lesson-15/lesson-15.js
+++ src/lesson-15/lesson-15.js
@@ -1,7 +1,11 @@
 import {Lamp} from "./component/lamp/lamp";
 import {LampManager} from "./component/lamp-manager/lamp-manager";
+import {TaskList} from "./component/taskList/task-list";
 
-
-new Lamp(document.querySelector("#lamp"));
 
 new LampManager(document.querySelector('#manager'));
+
+
+window.lamp = new Lamp(document.querySelector('#lamp'));
+
+new TaskList(document.querySelector('#task-list'));
